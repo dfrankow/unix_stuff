05098085f9ab10640ea5d688a8cbc8bfd7fa1ae7
Dan Frankowski <dan@pinterest.com>
2016-03-10 17:55:01 -0800

    Use YAML instead

--- bin/configsub.py
+++ bin/configsub.py
@@ -2,38 +2,33 @@
 
 import re
 import sys
+import yaml
 
 """Read a config file and use it to substitute variables on stdin"""
 
 import argparse
 
 parser = argparse.ArgumentParser(
-description='Read a config file and use it to substitute variables on stdin.')
+    description='Read a config file and use it to substitute variables on stdin.')
 parser.add_argument('--config', metavar='FILE',
-                   help='Config file with variables', dest='config',
+                    help='Config file with variables', dest='config',
                     required=True)
 args = parser.parse_args()
 
-print args.config
-
-# Read the config file: lines of the form var=value
+# Read the YAML config file
+# TODO(dan): Figure out what to do with nested data structures (error out?)
 config = {}
+with open(args.config) as config_file:
+    config = yaml.load(config_file)
+
+# Precompute the regex patterns, why not
 config_pattern = {}
-for line in open(args.config).readlines():
-    line = line.rstrip('\n\r')
-    # skip empty lines and comments
-    if not line or line.startswith('#'):
-        continue
-
-    # Separate on first equals sign
-    var, value = line.split('=', 1)
-
-    config[var] = value
+for var, value in config.iteritems():
     config_pattern[var] = re.compile('@%s@' % var)
 
 # Substitute the pattern on stdin
 for line in sys.stdin.readlines():
     for var, value in config.iteritems():
         pattern = config_pattern[var]
-        line = pattern.sub(value, line)
+        line = pattern.sub(str(value), line)
     sys.stdout.write(line)
