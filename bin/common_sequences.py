f19279125f8ac8ae2b275910ac6cb45e6cdaed5a
Dan Frankowski <dan@pinterest.com>
2018-08-21 10:01:42 -0500

    Add thresholds

--- bin/common_sequences.py
+++ bin/common_sequences.py
@@ -8,36 +8,47 @@ Output is (tab-separated):
 sequence #-lines-with-sequence
 """
 
+# Use one of these
+TOPN_THRESHOLD = None
+COUNT_THRESHOLD = 1000
+
+
 import sys
 from collections import defaultdict
 
 
 def generate_ngrams(items, n):
     if len(items) < n:
-        yield items
+        yield tuple(items)
     else:
         # sliding window
         for idx in xrange(0, len(items)-n):
-            yield items[idx:(idx+n)]
+            yield tuple(items[idx:(idx+n)])
 
 
 # common 5-grams
 N=5
 the_map = defaultdict(int)
 for line in sys.stdin:
-    # all N-grams
+    # all N-grams, but only one counted per line
+    all = set([])
     for ngram in generate_ngrams(line.strip().split(' '), 5):
+        all.add(ngram)
+    for ngram in all:
         the_map[' '.join(ngram)] += 1
 
-
-# get the top 50 most common
-top_counts = sorted(the_map.values(), reverse=True)
-if len(top_counts) > 50:
-    top_counts = top_counts[:50]
+the_threshold = None
+if TOPN_THRESHOLD:
+    # get the most common
+    top_counts = sorted(the_map.values(), reverse=True)
+    if len(top_counts) > TOPN_THRESHOLD:
+        top_counts = top_counts[:TOPN_THRESHOLD]
+else:
+    the_threshold = COUNT_THRESHOLD
 
 entries = []
 for entry, count in the_map.iteritems():
-    if count >= top_counts[-1]:
+    if the_threshold is None or count >= the_threshold:
         entries.append((entry, count))
 
 for entry, count in sorted(entries, key=lambda x: x[1], reverse=True):
