aa08817146a355ea031ce77e78e3faa0bba51d6f
Dan Frankowski <dan@pinterest.com>
2018-08-21 10:01:01 -0500

    get more info, allow same stage # for different jobs

--- bin/hive_stage_times.py
+++ bin/hive_stage_times.py
@@ -11,26 +11,57 @@ import datetime
 begin_map = {}
 end_map = {}
 reduce_begin_map = {}
+num_mappers_map = {}
+num_reducers_map = {}
+stage_map = {}
+# job_map: map current stage => job.  could change for lots of jobs.
+job_map = {}
 
 # the order in which the stages were encountered
 for line in sys.stdin:
-    m = re.search(r'((\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d),\d\d\d (Stage-\d+) map = \d+%,  reduce = (\d+)%)', line)
+    m = re.search(r'Hadoop job information for (Stage-\d+): number of mappers: (\d+); number of reducers: (\d+)', line)
     if m:
-        dt, stage, reduce_percent = m.group(2), m.group(3), m.group(4)
-        if stage not in begin_map:
+        stage, num_mappers, num_reducers = m.group(1), m.group(2), m.group(3)
+        job = job_map[stage]
+        num_mappers_map[job] = num_mappers
+        num_reducers_map[job] = num_reducers
+
+    m = re.search(r'Starting Job = ([^,]+), Stage = (Stage-\d+)', line)
+    if m:
+        job, stage = m.group(1), m.group(2)
+        job_map[stage] = job
+        stage_map[job] = stage
+
+    # m = re.search(r'Kill Command = /usr/local/hadoop/bin/hadoop job  -kill ([^ ]+)', line)
+
+    m = re.search(r'((\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d),\d\d\d (Stage-\d+) map = (\d+)%,  reduce = (\d+)%)', line)
+    if m:
+        dt, stage, map_percent, reduce_percent = (
+            m.group(2), m.group(3), m.group(4), m.group(5))
+        job = job_map[stage]
+        if job not in begin_map:
             # print "stage '%s' dt '%s', line %s" % (stage, dt, line)
-            begin_map[stage] = dt
-        if int(reduce_percent) > 0 and stage not in reduce_begin_map:
+            begin_map[job] = dt
+        if int(reduce_percent) > 0 and job not in reduce_begin_map:
             # print "stage '%s' dt '%s' reduce %s, line %s" % (stage, dt, reduce_percent, line)
-            reduce_begin_map[stage] = dt
-        end_map[stage] = dt
+            reduce_begin_map[job] = dt
+        end_map[job] = dt
 
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 
-for stage in sorted(begin_map.keys(), cmp=lambda x,y: cmp(begin_map[x], begin_map[y])):
-    begin_dt = begin_map[stage]
-    end_dt = end_map.get(stage)
-    reduce_begin_dt = reduce_begin_map.get(stage)
+print '\t'.join(['stage', 'start', 'end',
+                 'time', 'reduce_time',
+                 'mappers', 'reducers',
+                 'job'])
+
+for job in sorted(begin_map.keys(), cmp=lambda x,y: cmp(begin_map[x], begin_map[y])):
+    stage = stage_map[job]
+    num_mappers = num_mappers_map[job]
+    num_reducers = num_reducers_map[job]
+
+    begin_dt = begin_map[job]
+    end_dt = end_map.get(job)
+    reduce_begin_dt = reduce_begin_map.get(job)
 
     begin_time = datetime.datetime.strptime(begin_dt, DATETIME_FORMAT)
     end_time = datetime.datetime.strptime(end_dt, DATETIME_FORMAT)
@@ -38,7 +69,11 @@ for stage in sorted(begin_map.keys(), cmp=lambda x,y: cmp(begin_map[x], begin_ma
     tdiff = end_time - begin_time
     reduce_diff = end_time - reduce_begin_time
 
-    print ' '.join([stage, begin_dt, 'to', end_map.get(stage),
-                    "%.1f hours" % (tdiff.total_seconds() / 60.0 / 60),
-                    'reduce began', reduce_begin_dt,
-                    "%.1f hours" % (reduce_diff.total_seconds() / 60.0 / 60)])
+    print '\t'.join([stage, begin_dt, end_dt,
+                     "%.1f hours" % (tdiff.total_seconds() / 60.0 / 60),
+                     # 'reduce began', reduce_begin_dt,
+                     "%.1f hours" % (reduce_diff.total_seconds() / 60.0 / 60),
+                     num_mappers,
+                     num_reducers,
+                     job
+    ])
