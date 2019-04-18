b71d5f429b090b3b3de369a208b66cf98da9c24d
Dan Frankowski <34655+dfrankow@users.noreply.github.com>
2019-04-18 15:28:13 -0500

    Add --whole-file to pretty_print_json.py

--- bin/pretty_print_json.py
+++ bin/pretty_print_json.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python2.7
 
+import argparse
 import json
 import sys
 
-for line in sys.stdin:
-    the_obj = json.loads(line)
+parser = argparse.ArgumentParser(
+    description='Read a file and pretty-print the json')
+parser.add_argument('--whole-file', action='store_true',
+                    help='If given, grab the whole file as one JSON blog',
+                    dest='whole_file')
+args = parser.parse_args()
+
+if args.whole_file:
+    the_obj = json.loads(sys.stdin.read())
     print json.dumps(the_obj, sort_keys = True, indent=2)
+else:
+    for line in sys.stdin:
+        the_obj = json.loads(line)
+        print json.dumps(the_obj, sort_keys = True, indent=2)
