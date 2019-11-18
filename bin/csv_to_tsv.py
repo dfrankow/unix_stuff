abf7362946893ec5f2862b91242265dd6cdfed3c
Dan Frankowski <dfrankow@gmail.com>
2019-11-18 15:15:18 -0600

    More tolerant csv_to_tsv.py

--- bin/csv_to_tsv.py
+++ bin/csv_to_tsv.py
@@ -8,6 +8,13 @@ import csv
 import re
 import sys
 
+# Python 3.7 and newer
+# This encoding is more tolerant than utf-8:
+# sys.stdin.reconfigure(encoding='cp1252')
+
+# See also https://stackoverflow.com/a/15063941/34935
+csv.field_size_limit(sys.maxsize)
+
 csvreader = csv.reader(sys.stdin, delimiter=',')
 for row in csvreader:
     for idx in range(len(row)):
