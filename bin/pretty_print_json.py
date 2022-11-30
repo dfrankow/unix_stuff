4ea0e98a88a6177d722cda0988463487d9bc130b
Dan Frankowski <34655+dfrankow@users.noreply.github.com>
2022-11-30 11:35:07 -0600

    Add --sort-keys

--- bin/pretty_print_json.py
+++ bin/pretty_print_json.py
@@ -7,14 +7,18 @@ import sys
 parser = argparse.ArgumentParser(
     description='Read a file and pretty-print the json')
 parser.add_argument('--whole-file', action='store_true',
-                    help='If given, grab the whole file as one JSON blog',
-                    dest='whole_file')
+                    help='If given, grab the whole file as one JSON blog')
+parser.add_argument('--sort-keys', action=argparse.BooleanOptionalAction,
+                    help='sort the keys',
+                    default=True)
 args = parser.parse_args()
 
 if args.whole_file:
     the_obj = json.loads(sys.stdin.read())
-    print(json.dumps(the_obj, sort_keys = True, indent=2, ensure_ascii=False))
+    print(json.dumps(the_obj, sort_keys = args.sort_keys,
+                     indent=2, ensure_ascii=False))
 else:
     for line in sys.stdin:
         the_obj = json.loads(line)
-        print(json.dumps(the_obj, sort_keys = True, indent=2, ensure_ascii=False))
+        print(json.dumps(the_obj, sort_keys = args.sort_keys,
+                         indent=2, ensure_ascii=False))
