8f37a1376bed779375fcb5105f94ab2753d6001a
Dan Frankowski <34655+dfrankow@users.noreply.github.com>
2023-01-11 07:37:48 -0600

    Add --verbose to jaccard_index.py

--- bin/jaccard_index.py
+++ bin/jaccard_index.py
@@ -13,30 +13,53 @@ $ jaccard_index.py <(head -25 file1) <(head -40 file2)
 # in union: 56
 jaccard index: 0.16071428571428573
 """
-
+import argparse
 import sys
 
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: {sys.argv[0]} file1 file2")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description='Compare dicts')
 
-    with open(sys.argv[1]) as the_file:
+    parser.add_argument('file1', help='File 1')
+    parser.add_argument('file2', help='File 2')
+    parser.add_argument('--verbose', '-v', action='store_true', help='Verbose')
+    args = parser.parse_args()
+
+    with open(args.file1) as the_file:
         lines1 = set(the_file.readlines())
 
-    with open(sys.argv[2]) as the_file:
+    with open(args.file2) as the_file:
         lines2 = set(the_file.readlines())
 
-    intersection = len(lines1.intersection(lines2))
-    union = len(lines1.union(lines2))
-    jaccard = intersection / float(union) if union else float('nan')
+    intersection = lines1.intersection(lines2)
+    intersection_len = len(intersection)
+    union = lines1.union(lines2)
+    union_len = len(union)
+    jaccard = intersection_len / float(union_len) if union_len else float('nan')
 
     print(f"# unique lines of file1: {len(lines1)}")
     print(f"# unique lines of file2: {len(lines2)}")
-    print(f"# in intersection: {intersection}")
-    print(f"# in union: {union}")
+    print(f"# in intersection: {intersection_len}")
+    print(f"# in union: {union_len}")
     print(f"jaccard index: {jaccard}")
+
+    if args.verbose:
+        print()
+        # Output like comm
+        lines1 = sorted(lines1)
+        lines2 = sorted(lines2)
+        for line in (list(intersection) + list(set(lines1)-intersection)
+                + list(set(lines2)-intersection)):
+            line1 = line.replace('\n', '')
+            print(line1, "\t", end='')
+            if line in intersection:
+                print("both")
+            elif line in lines1:
+                print("file1")
+            else:
+                assert line in lines2
+                print("file2")
+
 
 if __name__ == '__main__':
     main()
