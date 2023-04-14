4a030e3dbe2feac85154886786c5881ba152c7c9
Dan Frankowski <dfrankow@gmail.com>
2023-04-14 14:15:30 -0500

    Add --no-indent

--- bin/pretty_print_json.py
+++ bin/pretty_print_json.py
@@ -11,14 +11,19 @@ parser.add_argument('--whole-file', action='store_true',
 parser.add_argument('--sort-keys', action=argparse.BooleanOptionalAction,
                     help='sort the keys',
                     default=False)
+parser.add_argument('--no-indent', action='store_true',
+                    help="If True, use the most compact representation",
+                    default=False)
 args = parser.parse_args()
+
+indent = None if args.no_indent else 2
 
 if args.whole_file:
     the_obj = json.loads(sys.stdin.read())
     print(json.dumps(the_obj, sort_keys = args.sort_keys,
-                     indent=2, ensure_ascii=False))
+                     indent=indent, ensure_ascii=False))
 else:
     for line in sys.stdin:
         the_obj = json.loads(line)
         print(json.dumps(the_obj, sort_keys = args.sort_keys,
-                         indent=2, ensure_ascii=False))
+                         indent=indent, ensure_ascii=False))
